46f418bc8835a612caedfa69b6389943a9865cab
krasi <krasimir.xristov@gmail.com>
2025-11-09 18:09:15 +0100

    Add FastAPI application and update .gitignore for environment files

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+text_posts = {1: {"title": "New post", "content": "This is my first post"},}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return text_posts.get(id)
